8d39089e8b0ab89d9862e34385eeb04bd0a64df7
vic7or019 <50488316+vic7or019@users.noreply.github.com>
2024-11-29 21:08:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 
 TOKEN = '7300877680:AAFMDFouNAdvJXD3n8akwUBqyPUQ_Xz2iaQ'
-PAYMENT_SERVER_URL = 'http://84.201.180.71:5000/payment'
+PAYMENT_SERVER_URL = 'http://84.201.180.71/payment'  # Убран порт 5000
 CHAT_IDS_FILE = "chat_ids.txt"
 
 
