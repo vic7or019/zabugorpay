bedd4884ee1546f95338837793ea5db42b0d4960
vic7or019 <50488316+vic7or019@users.noreply.github.com>
2024-11-29 20:12:45 +0300

    Update server.py

--- server.py
+++ server.py
@@ -74,4 +74,4 @@ def payment_status():
     return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)  # Порт изменён на 80
+    app.run(host='127.0.0.1', port=5000)
